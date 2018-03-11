4e55771eb422d79a32faaebee1943db6e6695ea7
Chupakabra111 <every.time.new555@gmail.com>
2018-03-11 12:15:08 +0400

    Jarangutyun

--- class_die.js
+++
@@ -1,8 +0,0 @@
-die() {
-    matrix[this.y][this.x] = 0;
-    for (var i in eatArr) {
-        if (this.x == eatArr[i].x && this.y == eatArr[i].y) {
-            eatArr.splice(i, 1);
-        }
-    }
-}
